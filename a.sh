4ee0dfde6833033f1546df3de40248514912c14d
divyap2808 <163121007+divyap2808@users.noreply.github.com>
2024-03-12 16:19:37 +0530

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/ksh
+#My first Shell Scrpt
+#Author:Divya P
+#Version:1.0
+ print "Enter your name: "
+read name
+print "enter department:"
+read dept
+print "HI ${name} Welcome to ${dept} department"
